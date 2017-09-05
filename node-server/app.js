04fcf7e780a67e6540217b8db9e90238637b9e25
baozefeng <727751065@qq.com>
2017-09-05 16:55:12 +0800

    test wx

--- node-server/app.js
+++ node-server/app.js
@@ -13,28 +13,29 @@ app.use('/api', function (req, res) {
   req.pipe(request(url)).pipe(res);
 });
 
-app.use('/wx', function (req, res) {
-  const token = 'schooltrip';
-  // console.log(req)
-  const params = req.query;
-  console.log(params);
-  const signature = params.signature;
-  const timestamp = params.timestamp;
-  const nonce = params.nonce;
-  let tmpArr = [token, timestamp, nonce];
-  tmpArr.sort();
-  let sha1 = crypto.createHash('sha1');
-  let msg = tmpArr[0] + tmpArr[1] + tmpArr[2];
-  sha1.update(msg);
-  msg = sha1.digest('hex');
-  if (msg == signature) {
-    console.log('验证成功');
-    res.send(query.echostr);
-  } else {
-    console.log('验证失败');
-    res.send('微信登录验证失败，请重试！');
-  }
-})
+app.use('/wx', express.static('../fontend/dist/index.html'));
+// app.use('/wx', function (req, res) {
+//   const token = 'schooltrip';
+//   // console.log(req)
+//   const params = req.query;
+//   console.log(params);
+//   const signature = params.signature;
+//   const timestamp = params.timestamp;
+//   const nonce = params.nonce;
+//   let tmpArr = [token, timestamp, nonce];
+//   tmpArr.sort();
+//   let sha1 = crypto.createHash('sha1');
+//   let msg = tmpArr[0] + tmpArr[1] + tmpArr[2];
+//   sha1.update(msg);
+//   msg = sha1.digest('hex');
+//   if (msg == signature) {
+//     console.log('验证成功');
+//     res.send(query.echostr);
+//   } else {
+//     console.log('验证失败');
+//     res.send('微信登录验证失败，请重试！');
+//   }
+// })
 
 app.listen(80, function () {
   console.log('Example app listening on port 3000!')
