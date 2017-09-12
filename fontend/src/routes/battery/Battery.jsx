610f0dc9a2091ff20cab794908c678626668b7d8
baozefeng <727751065@qq.com>
2017-09-12 17:05:45 +0800

    pay test

--- fontend/src/routes/battery/Battery.jsx
+++ fontend/src/routes/battery/Battery.jsx
@@ -107,10 +107,10 @@ class Battery extends Component {
   wxpay() {
     wx.chooseWXPay({
       timestamp: parseInt(new Date().getTime() / 1000) + '', // 支付签名时间戳，注意微信jssdk中的所有使用timestamp字段均为小写。但最新版的支付后台生成签名使用的timeStamp字段名需大写其中的S字符
-      nonceStr: 'CX1BEOuZJJ8v5xDS', // 支付签名随机串，不长于 32 位
-      package: 'prepay_id=wx20170912153529308eb36ac90335591882', // 统一支付接口返回的prepay_id参数值，提交格式如：prepay_id=***）
+      nonceStr: 'UFsSVgsTXz77VnpQ', // 支付签名随机串，不长于 32 位
+      package: 'prepay_id=wx20170912170420eb006a8e670744861202', // 统一支付接口返回的prepay_id参数值，提交格式如：prepay_id=***）
       signType: 'MD5', // 签名方式，默认为'SHA1'，使用新版支付需传入'MD5'
-      paySign: '3B278361C28E556E2F2287EED9420FDB', // 支付签名
+      paySign: 'D60E5D7F83734565F2061492579E364C', // 支付签名
       success: function (res) {
         // 支付成功后的回调函数
         alert('suceess' + res);
