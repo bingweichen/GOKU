f35ce543b7d43cec8c0f743c60372ba0b5418553
bingweiChen <bingwei.chen11@gmail.com>
2017-09-12 15:20:34 +0800

    fix logs error

--- backend/server/wx/configure.py
+++ backend/server/wx/configure.py
@@ -1,2 +1,13 @@
-appId = "wx0350234b8e970c00"
-appSecret = "97dfd4483fd75d568da28d3ea9508632"
+appId = "wx79012d734b31d795"
+appSecret = "1de216b8d2b6f543c6b4778fb040100e"
+
+
+# =======【基本信息设置】=====================================
+# 微信公众号身份的唯一标识。审核通过后，在微信发送的邮件中查看
+APPID = "wx79012d734b31d795"
+# JSAPI接口中获取openid，审核后在公众平台开启开发模式后可查看
+APPSECRET = "1de216b8d2b6f543c6b4778fb040100e"
+# 受理商ID，身份标识
+MCHID = "1485893242"
+# 商户支付密钥Key。审核通过后，在微信发送的邮件中查看
+KEY = "schooltripscomcnschooltripscomcn"
