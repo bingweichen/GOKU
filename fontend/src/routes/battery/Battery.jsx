9fbcbbf427736eef6f816bc7654e0695bf7a25df
baozefeng <727751065@qq.com>
2017-09-12 21:08:18 +0800

    pay test

--- fontend/src/routes/battery/Battery.jsx
+++ fontend/src/routes/battery/Battery.jsx
@@ -106,11 +106,11 @@ class Battery extends Component {
 
   wxpay() {
     wx.chooseWXPay({
-      timestamp: parseInt(new Date().getTime() / 1000) + '', // 支付签名时间戳，注意微信jssdk中的所有使用timestamp字段均为小写。但最新版的支付后台生成签名使用的timeStamp字段名需大写其中的S字符
-      nonceStr: 'UFsSVgsTXz77VnpQ', // 支付签名随机串，不长于 32 位
-      package: 'prepay_id=wx20170912170420eb006a8e670744861202', // 统一支付接口返回的prepay_id参数值，提交格式如：prepay_id=***）
+      timestamp: '1505192699', // 支付签名时间戳，注意微信jssdk中的所有使用timestamp字段均为小写。但最新版的支付后台生成签名使用的timeStamp字段名需大写其中的S字符
+      nonceStr: '3n6jal5rsl05i2yhgxfssm34rk5nmkib', // 支付签名随机串，不长于 32 位
+      package: 'prepay_id=wx20170912210459124d1850920376751626', // 统一支付接口返回的prepay_id参数值，提交格式如：prepay_id=***）
       signType: 'MD5', // 签名方式，默认为'SHA1'，使用新版支付需传入'MD5'
-      paySign: 'D60E5D7F83734565F2061492579E364C', // 支付签名
+      paySign: '91C319ED4BAEB9B08DA35182D259C35E', // 支付签名
       success: function (res) {
         // 支付成功后的回调函数
         alert('suceess' + res);
